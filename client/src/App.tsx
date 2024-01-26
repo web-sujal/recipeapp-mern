0d495e5fcbf6ba9d0253b57ea3a9fc63674917b4
Sujal <web.sujal@gmail.com>
2024-01-26 23:35:20 +0530

    setup project for client dir and add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,46 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import AuthLayout from "./layouts/AuthLayout";
+import RootLayout from "./layouts/RootLayout";
+import CreateRecipe from "./pages/CreateRecipe";
+import SavedRecipes from "./pages/SavedRecipes";
+
 function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      HELLO chutiya WORLD!
-    </h1>
+    <Routes>
+      {/* Public Routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+
+      {/* Private Routes */}
+      <Route element={<RootLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-recipe" element={<CreateRecipe />} />
+        <Route path="/saved-recipes" element={<SavedRecipes />} />
+      </Route>
+    </Routes>
   );
 }
 
 export default App;
+
+/*
+  src/
+    components/
+    layouts/
+      AuthLayout.tsx
+      RootLayout.tsx
+    pages/ 
+      CreateRecipe.tsx
+      Home.tsx
+      Login.tsx
+      SavedRecipes.tsx
+      Signup.tsx
+    App.tsx
+    index.css
+    main.tsx
+*/
