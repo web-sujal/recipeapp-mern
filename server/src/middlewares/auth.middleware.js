a22d5bbcfa7f5b3972c926ed1ed7d43cd03a9543
Sujal <web.sujal@gmail.com>
2024-01-31 19:47:36 +0530

    add verifyJWT middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import { User } from "../models/User.model.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("bearer ", "");
+
+    if (!token) throw new ApiError(401, "Unauthorized request!");
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) throw new ApiError(401, "Invalid Access Token!");
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(new ApiError(401, error?.message || "Invalid access token"));
+  }
+};
--- server/src/routes/recipe.routes.js
+++ server/src/routes/recipe.routes.js
@@ -6,14 +6,17 @@ import {
   fetchSavedRecipes,
   saveRecipe,
 } from "../controllers/recipe.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/").get(fetchAllRecipes);
-router.route("/create-recipe").post(createRecipe);
-router.route("/save-recipe").post(saveRecipe);
 
-router.route("/saved-recipes/ids/:userId").get(fetchSavedRecipeIds);
-router.route("/saved-recipes/:userId").get(fetchSavedRecipes);
+// PROTECTED ROUTES
+router.route("/create-recipe").post(verifyJWT, createRecipe);
+router.route("/save-recipe").post(verifyJWT, saveRecipe);
+
+router.route("/saved-recipes/ids/:userId").get(verifyJWT, fetchSavedRecipeIds);
+router.route("/saved-recipes/:userId").get(verifyJWT, fetchSavedRecipes);
 
 export { router as recipeRouter };
